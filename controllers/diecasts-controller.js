a05de8985fd4e034c3a05da09314b00dd813ee47
Ryan Bey <bey15002@byui.edu>
2022-05-19 13:55:19 -0600

    added diecasts controller

--- controllers/diecasts-controller.js
+++ controllers/diecasts-controller.js
@@ -1,1 +1,31 @@
-// Diecasts controller
+const connect = require('../db/connect');
+// const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+   const result = connect.getCollection().find();
+   result.toArray().then((documents) => {
+      res.status(200).json(documents);
+   });
+};
+
+const addDiecast = async (req, res) => {
+   const diecast = {
+      carNumber: req.body.carNumber,
+      make: req.body.make,
+      model: req.body.model,
+      year: req.body.year,
+      scale: req.body.scale,
+      sponsor: req.body.sponsor,
+      driverFirst: req.body.driverFirst,
+      driverLast: req.body.driverLast,
+      imageUrl: req.body.imageUrl,
+   };
+   const response = await connect.getCollection().insertOne(diecast);
+   if (response.acknowledged) res.status(201).json(response);
+   else res.status(500).json(response.error || 'Error creating the diecast.');
+};
+
+module.exports = {
+   getAll,
+   addDiecast,
+};
--- routes/diecasts.js
+++ routes/diecasts.js
@@ -1,46 +1,8 @@
 const routes = require('express').Router();
-const connect = require('../db/connect');
+const diecastsController = require('../controllers/diecasts-controller');
 // const ObjectId = require('mongodb').ObjectId;
 
-// Get all diecasts
-routes.get('/', (req, res) => {
-   try {
-      const result = connect.getCollection().find();
-
-      result.toArray().then((documents) => {
-         res.status(200).json(documents);
-         console.log('Returned all diecasts!');
-      });
-   } catch (err) {
-      res.status(500).json(err);
-   }
-});
-
-// Add a diecast
-routes.post('/', async (req, res) => {
-   try {
-      const diecast = {
-         carNumber: req.body.carNumber,
-         make: req.body.make,
-         model: req.body.model,
-         year: req.body.year,
-         scale: req.body.scale,
-         sponsor: req.body.sponsor,
-         driverFirst: req.body.driverFirst,
-         driverLast: req.body.driverLast,
-         imageUrl: req.body.imageUrl
-      };
-
-      const response = await connect.getCollection().insertOne(diecast);
-
-      if (response.acknowledged) {
-         res.status(201).json(response);
-      } else {
-         res.status(500).json(response.error || 'Error creating the diecast.');
-      }
-   } catch (err) {
-      res.status(500).json(err);
-   }
-});
+routes.get('/', diecastsController.getAll);       // Get all diecasts
+routes.post('/', diecastsController.addDiecast);  // Add a diecast
 
 module.exports = routes;
