81369eccf3305a2ba679b483fda0b10e27f811d5
Ryan Bey <bey15002@byui.edu>
2022-05-19 14:04:20 -0600

    got swagger ui working

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router();
 
-// routes.use('/', require('./swagger'));
+routes.use('/', require('./swagger'));
 routes.get('/', (req, res) => { res.send('This is the home page wow'); })
 routes.use('/diecasts', require('./diecasts'));
 
