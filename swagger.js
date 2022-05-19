9fe54dd8b7d82828c41a83617ef0c3d859236974
Ryan Bey <bey15002@byui.edu>
2022-05-19 13:40:41 -0600

    added swagger files

---
+++ swagger.js
@@ -0,0 +1,23 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+   info: {
+      title: 'Display Case API',
+      description: 'API for NASCAR colectibles stored in my display case.'
+   },
+   // Test Locally: localhost:3000
+   // Use Heroku: display-case.herokuapp.com
+   host: 'localhost:3000',
+   schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// Generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//    await import('./index.js');
+// });
