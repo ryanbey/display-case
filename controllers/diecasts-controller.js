cc753220c6e17ea03837d23b7ef0e53cd1672a21
Ryan Bey <bey15002@byui.edu>
2022-05-26 17:10:48 -0600

    added put and delete routes and rest tests

--- controllers/diecasts-controller.js
+++ controllers/diecasts-controller.js
@@ -1,13 +1,24 @@
 const connect = require('../db/connect');
-// const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
+// Get all diecasts
+const getDiecasts = async (req, res) => {
    const result = connect.getCollection().find();
    result.toArray().then((documents) => {
       res.status(200).json(documents);
    });
 };
 
+// Get one diecast by id
+const getDiecast = async (req, res) => {
+   const diecastId = new ObjectId(req.params.id);
+   const result = connect.getCollection().find({ _id: diecastId });
+   result.toArray().then((documents) => {
+      res.status(200).json(documents[0]);
+   });
+};
+
+// Add a new diecast
 const addDiecast = async (req, res) => {
    const diecast = {
       carNumber: req.body.carNumber,
@@ -25,7 +36,37 @@ const addDiecast = async (req, res) => {
    else res.status(500).json(response.error || 'Error creating the diecast.');
 };
 
+// Edit a diecast
+const editDiecast = async (req, res) => {
+   const diecastId = new ObjectId(req.params.id);
+   const diecast = {
+      carNumber: req.body.carNumber,
+      make: req.body.make,
+      model: req.body.model,
+      year: req.body.year,
+      scale: req.body.scale,
+      sponsor: req.body.sponsor,
+      driverFirst: req.body.driverFirst,
+      driverLast: req.body.driverLast,
+      imageUrl: req.body.imageUrl,
+   };
+   const response = await connect.getCollection().replaceOne({ _id: diecastId }, diecast);
+   if (response.modifiedCount > 0) res.status(204).send();
+   else res.status(500).json(response.error || 'Error updating diecast');
+};
+
+// Delete a diecast
+const deleteDiecast = async (req, res) => {
+   const diecastId = new ObjectId(req.params.id);
+   const response = await connect.getCollection().remove({ _id: diecastId }, true);
+   if (response.deletedCount > 0) res.status(204).send();
+   else res.status(500).json(response.error || 'Error deleting contact');
+};
+
 module.exports = {
-   getAll,
+   getDiecasts,
+   getDiecast,
    addDiecast,
+   editDiecast,
+   deleteDiecast
 };
--- routes/diecasts.js
+++ routes/diecasts.js
@@ -1,13 +1,15 @@
 const routes = require('express').Router();
 const diecastsController = require('../controllers/diecasts-controller');
 const path = require('path');
-// const ObjectId = require('mongodb').ObjectId;
 
 // routes.get('/', (req, res) => {
 //    res.sendFile(path.join(__dirname + '/../views/diecasts.html'));
 // });
 
-routes.get('/', diecastsController.getAll);       // Get all diecasts
-routes.post('/', diecastsController.addDiecast);  // Add a diecast
+routes.get('/', diecastsController.getDiecasts);        // Get all diecasts
+routes.get('/:id', diecastsController.getDiecast)       // Get one diecast
+routes.post('/', diecastsController.addDiecast);        // Add a diecast
+routes.put('/:id', diecastsController.editDiecast)      // Edit a diecast
+routes.delete('/:id', diecastsController.deleteDiecast) // Delete a diecast
 
 module.exports = routes;
