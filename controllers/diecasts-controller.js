0251eafae8424dedab97dedc8ab453610a39bf48
Ryan Bey <bey15002@byui.edu>
2022-05-28 16:17:56 -0600

    added try catch to diecasts controller

--- controllers/diecasts-controller.js
+++ controllers/diecasts-controller.js
@@ -3,64 +3,97 @@ const ObjectId = require('mongodb').ObjectId;
 
 // Get all diecasts
 const getDiecasts = async (req, res) => {
-   const result = connect.getCollection().find();
-   result.toArray().then((documents) => {
-      res.status(200).json(documents);
-   });
+   try {
+      const result = connect.getCollection().find();
+      result.toArray().then((documents) => {
+         res.status(200).json(documents);
+      });
+   } catch (err) {
+      res.staus(500).json(err);
+   }
 };
 
 // Get one diecast by id
 const getDiecast = async (req, res) => {
-   const diecastId = new ObjectId(req.params.id);
-   const result = connect.getCollection().find({ _id: diecastId });
-   result.toArray().then((documents) => {
-      res.status(200).json(documents[0]);
-   });
+   try {
+      const diecastId = new ObjectId(req.params.id);
+      const result = connect.getCollection().find({ _id: diecastId });
+      result.toArray().then((documents) => {
+         res.status(200).json(documents[0]);
+      });
+   } catch (err) {
+      res.staus(500).json(err);
+   }
 };
 
 // Add a new diecast
 const addDiecast = async (req, res) => {
-   const diecast = {
-      carNumber: req.body.carNumber,
-      make: req.body.make,
-      model: req.body.model,
-      year: req.body.year,
-      scale: req.body.scale,
-      sponsor: req.body.sponsor,
-      driverFirst: req.body.driverFirst,
-      driverLast: req.body.driverLast,
-      imageUrl: req.body.imageUrl,
-   };
-   const response = await connect.getCollection().insertOne(diecast);
-   if (response.acknowledged) res.status(201).json(response);
-   else res.status(500).json(response.error || 'Error creating the diecast.');
+   try {
+      const diecast = {
+         carNumber: req.body.carNumber,
+         make: req.body.make,
+         model: req.body.model,
+         year: req.body.year,
+         scale: req.body.scale,
+         sponsor: req.body.sponsor,
+         driverFirst: req.body.driverFirst,
+         driverLast: req.body.driverLast,
+         imageUrl: req.body.imageUrl,
+      };
+
+      const response = await connect.getCollection().insertOne(diecast);
+
+      if (response.acknowledged) {
+         res.status(201).json(response);
+      } else {
+         res.status(500).json(response.error || 'Error creating the diecast.');
+      }
+   } catch (err) {
+      res.staus(500).json(err);
+   }
 };
 
 // Edit a diecast
 const editDiecast = async (req, res) => {
-   const diecastId = new ObjectId(req.params.id);
-   const diecast = {
-      carNumber: req.body.carNumber,
-      make: req.body.make,
-      model: req.body.model,
-      year: req.body.year,
-      scale: req.body.scale,
-      sponsor: req.body.sponsor,
-      driverFirst: req.body.driverFirst,
-      driverLast: req.body.driverLast,
-      imageUrl: req.body.imageUrl,
-   };
-   const response = await connect.getCollection().replaceOne({ _id: diecastId }, diecast);
-   if (response.modifiedCount > 0) res.status(204).send();
-   else res.status(500).json(response.error || 'Error updating diecast');
+   try {
+      const diecast = {
+         carNumber: req.body.carNumber,
+         make: req.body.make,
+         model: req.body.model,
+         year: req.body.year,
+         scale: req.body.scale,
+         sponsor: req.body.sponsor,
+         driverFirst: req.body.driverFirst,
+         driverLast: req.body.driverLast,
+         imageUrl: req.body.imageUrl,
+      };      
+      
+      const diecastId = new ObjectId(req.params.id);
+      const response = await connect.getCollection().replaceOne({ _id: diecastId }, diecast);
+
+      if (response.modifiedCount > 0) {
+         res.status(204).send();
+      } else {
+         res.status(500).json(response.error || 'Error updating diecast');
+      }
+   } catch (err) {
+      res.staus(500).json(err);
+   }
 };
 
 // Delete a diecast
 const deleteDiecast = async (req, res) => {
-   const diecastId = new ObjectId(req.params.id);
-   const response = await connect.getCollection().remove({ _id: diecastId }, true);
-   if (response.deletedCount > 0) res.status(204).send();
-   else res.status(500).json(response.error || 'Error deleting contact');
+   try {
+      const diecastId = new ObjectId(req.params.id);
+      const response = await connect.getCollection().remove({ _id: diecastId }, true);
+      if (response.deletedCount > 0) {
+         res.status(204).send();
+      } else {
+          res.status(500).json(response.error || 'Error deleting contact');
+      }
+   } catch (err) {
+      res.staus(500).json(err);
+   }
 };
 
 module.exports = {
