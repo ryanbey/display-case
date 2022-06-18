2d11dce74db495225e880deac62e412c96f8e43c
Ryan Bey <bey15002@byui.edu>
2022-06-18 16:18:57 -0600

    fixing heroku errors

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const connect = require('./db/connect');
+const connect = require('./db/connect.ts');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
